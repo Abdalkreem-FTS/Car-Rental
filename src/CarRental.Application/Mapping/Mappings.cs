36b126f659299c55925aa662ad5ec47019d4b251
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-08-28 17:29:52 +0300

    Generate the response mappings with Riok Mapperly

--- src/CarRental.Application/Mapping/Mappings.cs
+++ src/CarRental.Application/Mapping/Mappings.cs
@@ -3,71 +3,19 @@ using CarRental.Application.Contracts.Cars;
 using CarRental.Application.Contracts.Profile;
 using CarRental.Application.Contracts.Reservations;
 using CarRental.Domain.Entities;
+using Riok.Mapperly.Abstractions;
 
 namespace CarRental.Application.Mapping;
 
-public static class Mappings
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public static partial class Mappings
 {
-    public static CarResponse ToResponse(this Car car) => new(
-        car.Id,
-        car.Make,
-        car.Model,
-        car.Year,
-        car.PlateNumber,
-        car.Location,
-        car.DailyRate,
-        car.Seats,
-        car.Category,
-        car.Transmission,
-        car.Fuel,
-        car.ImageUrl,
-        car.Description,
-        car.IsActive);
+    public static partial CarResponse ToResponse(this Car car);
 
-    extension(ApplicationUser user)
-    {
-        public UserResponse ToResponse(IReadOnlyList<string> roles) => new(
-            user.Id,
-            user.FirstName,
-            user.LastName,
-            user.Email ?? string.Empty,
-            user.PhoneNumber ?? string.Empty,
-            roles);
+    public static partial UserResponse ToResponse(this ApplicationUser user, IReadOnlyList<string> roles);
 
-        public ProfileResponse ToProfileResponse(IReadOnlyList<string> roles) => new(
-            user.Id,
-            user.FirstName,
-            user.LastName,
-            user.Email ?? string.Empty,
-            user.PhoneNumber ?? string.Empty,
-            user.DateOfBirth,
-            user.AddressLine1,
-            user.AddressLine2,
-            user.City,
-            user.Country,
-            user.DriverLicenseNumber,
-            user.CreatedAtUtc,
-            roles);
-    }
-
-    public static ReservationResponse ToResponse(this Reservation reservation)
-    {
-        var car = reservation.Car;
-
-        return new ReservationResponse(
-            reservation.Id,
-            reservation.CarId,
-            car?.Make ?? string.Empty,
-            car?.Model ?? string.Empty,
-            car?.Year ?? 0,
-            car?.ImageUrl,
-            reservation.StartDate,
-            reservation.EndDate,
-            reservation.TotalDays,
-            car?.DailyRate ?? 0m,
-            reservation.TotalPrice,
-            reservation.Status,
-            reservation.PickupLocation,
-            reservation.CreatedAtUtc);
-    }
+    public static partial ProfileResponse ToProfileResponse(this ApplicationUser user, IReadOnlyList<string> roles);
+    
+    [MapProperty("Car.DailyRate", nameof(ReservationResponse.DailyRate))]
+    public static partial ReservationResponse ToResponse(this Reservation reservation);
 }
